4bd1cce92ef2fcfd6e6ab151f34ae68e33e8258b
harshcoffee <austincapps@fastmail.fm>
2019-06-01 15:13:36 -0500

    mousePosition

---
+++ JavaScript Practice/displayMousePosition.js
@@ -0,0 +1,30 @@
+var isMouseTracking = false;
+
+/* We need a function to update
+On our front end, we have 2 labels -
+one for x, one for y */
+function updateMousePosition() {
+  if (isMouseTracking) {
+    //We can get the mouse positions with
+    //event.clientx and event.clienty
+    //We can update the text of an HTML element with the
+    //innerText property
+    var positionX = document.getElementById("mousePositionX");
+    positionX.innerText = event.clientX;
+    var positionY = document.getElementById("mousePositionY");
+    positionY.innerText = event.clientY;
+    }
+}
+
+//We need a function to toggle whether tracking is enabled
+function toggleTracking() {
+  //If isMouseTracking is true, set it to false
+  //Otherwise it is false, and needs to be set to true
+  isMouseTracking = !isMouseTracking;
+  //Update the tracking status to show whether it is enabled
+  if (isMouseTracking) {
+    document.getElementById("trackingStatus").innerText = "TRACKING";
+  } else {
+    document.getElementById("trackingStatus").innerText = "NOT TRACKING";
+  }
+}

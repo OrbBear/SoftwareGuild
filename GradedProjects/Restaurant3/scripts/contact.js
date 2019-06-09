cb44e0b4d9a1089a981deb25107bfb5205d56351
harshcoffee <austincapps@fastmail.fm>
2019-06-09 12:33:46 -0500

    r3DoneExceptTheme

--- GradedProjects/Restaurant3/scripts/contact.js
+++ GradedProjects/Restaurant3/scripts/contact.js
@@ -7,9 +7,7 @@ function validateItems() {
   var reason = document.forms["contactForm"]["reason"].value;
   var info = document.forms["contactForm"]["info"].value;
   var beenTo = document.forms["contactForm"]["beenTo"].value;
-  //Multiple checkboxes with the same name causes form to come back blank
-  //Gave each day a different name and combine here for validation
-  var contact = checkorheck();
+  var contact = checkedOrNot();
   if (name != "" && email != "" && phone != "" && reason != "" && info != "" && beenTo != "" && contact != "" ) {
     //Add form submitted after backend is in place.
     alert("Information is Valid!");
@@ -25,7 +23,7 @@ function validateItems() {
 //Workaround as document.forms["contractForm"]["contract"] always returned blank.
 //Checks to see if at least one checkbox is checked.
 //If at least one is checked returns true if none returns false
-function checkorheck(){
+function checkedOrNot(){
   var checkIds = ["monday", "tuesday", "wednesday", "thursday", "friday"];
   var result = false;
   for (var i = 0; i < checkIds.length; i++){
